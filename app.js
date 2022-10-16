51f0e7ac750334633bc8696f3d240a35e5d26881
WolfWST1 <garcia.moreno.emiliano4@gmail.com>
2022-10-15 23:16:40 -0500

    commita

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/Hello_world.html')
+})
+
+app.get('/form', (req, res)=>{
+    res.sendFile(__dirname + '/form.html')
+})
+
+app.listen(3000)
+console.log('server on port 3000')
